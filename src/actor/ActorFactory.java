0f8081cd274a00d8c0a172c8a86a9f5ce714e9df
Neurolytic <astrada2023@gmail.com>
2015-05-20 17:02:15 -0500

    Fix ActorFactory crash on invalid ID string.

--- src/actor/ActorFactory.java
+++ src/actor/ActorFactory.java
@@ -3,7 +3,11 @@ package actor;
 public class ActorFactory {
 
   public static Actor makeActor(String actorTemplateID) {
-    return new Actor(ActorTemplate.LIB.get(actorTemplateID.toUpperCase()));
+    ActorTemplate actorTemplate = ActorTemplate.LIB.get(actorTemplateID.toUpperCase());
+    if (actorTemplate != null) {
+      return new Actor(actorTemplate);
+    }
+    return null;
   }
 
 }

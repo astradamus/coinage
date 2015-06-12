414dedfe00ebf27786400b76bd0cde25579064d9
Alexander Strada <astrada2023@gmail.com>
2015-06-11 23:09:46 -0500

    Implement Attribute.Reflex-based movement speeds.

--- src/controller/action/Moving.java
+++ src/controller/action/Moving.java
@@ -1,6 +1,7 @@
 package controller.action;
 
 import actor.Actor;
+import actor.attribute.Attribute;
 import game.Direction;
 import game.Game;
 import world.Coordinate;
@@ -15,9 +16,25 @@ public class Moving extends Action {
     addFlag(ActionFlag.REPEAT_ON_SUCCESS);
   }
 
+
+  /*
+   * At BASELINE_RANK reflex, an actor will suffer a BEATS_AT_BASELINE recovery. For every
+   * DISTANCE_FROM_BASELINE_DIVISOR ranks above or below BASELINE_RANK, the actor suffers one
+   * less or one more beat of recovery, respectively.
+   */
+  private final static int BASELINE_RANK = 5;
+  private final static int BEATS_AT_BASELINE = 5;
+  private final static int DISTANCE_FROM_BASELINE_DIVISOR = 2;
+
   @Override
   protected int calcBeatsToPerform() {
-    return 3;
+
+    int actorReflex = getActor().readAttributeLevel(Attribute.REFLEX).ordinal();
+
+    int distanceFromBaseline = actorReflex - BEATS_AT_BASELINE;
+
+    return BASELINE_RANK - distanceFromBaseline / DISTANCE_FROM_BASELINE_DIVISOR;
+
   }
 
   @Override

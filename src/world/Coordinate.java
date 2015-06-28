c6358a99a003c73d814f9dd2156dfea2518cf599
Alexander Strada <astrada2023@gmail.com>
2015-06-28 18:00:11 -0500

    Add several convenience methods to Coordinate.
     - All take another coordinate and return some information about the relationship between the two. getGlobalDistance(), getWorldDistance(), getDirectionTo(), getIsAdjacentTo().

--- src/world/Coordinate.java
+++ src/world/Coordinate.java
@@ -1,5 +1,8 @@
 package world;
 
+import game.Direction;
+import utils.Utils;
+
 /**
  *
  */
@@ -25,6 +28,50 @@ public class Coordinate {
     this.localX = localX;
     this.localY = localY;
   }
+
+  /**
+   * @return The Chebyshev/"Chessboard" distance between this and another coordinate, with units
+   * measured in individual game squares.
+   */
+  public int getGlobalDistance(Coordinate target) {
+
+    final int deltaX = Math.abs(this.globalX-target.globalX);
+    final int deltaY = Math.abs(this.globalY-target.globalY);
+
+    return Math.max(deltaX, deltaY);
+
+  }
+
+  /**
+   * @return The Chebyshev/"Chessboard" distance between this and another coordinate, with units
+   * measured in whole game areas.
+   */
+  public int getWorldDistance(Coordinate target) {
+
+    final int deltaX = Math.abs(this.worldX-target.worldX);
+    final int deltaY = Math.abs(this.worldY-target.worldY);
+
+    return Math.max(deltaX, deltaY);
+
+  }
+
+  /**
+   * @return The direction from this to another coordinate. Only due north is considered north--one
+   * square west or east becomes northwest or northeast, and so on for each other direction. This
+   * means the range considered "northwest" is a lot bigger than that considered "north" or "west"
+   * individually.
+   */
+  public Direction getDirectionTo(Coordinate target) {
+    return Direction.fromPointToPoint(this.globalX, this.globalY, target.globalX, target.globalY);
+  }
+
+  public boolean getIsAdjacentTo(Coordinate target) {
+    return Utils.getPointsAreAdjacent(
+        this.globalX, this.globalY,
+        target.globalX, target.globalY);
+  }
+
+
 
   public Square getSquare() {
     return area.getSquare(this);

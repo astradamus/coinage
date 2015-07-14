cee314d11a99ffa7eff508167f02c4dd0eb742d8
Alexander Strada <astrada2023@gmail.com>
2015-07-14 17:55:55 -0500

    refactor(Biome): use containers instead of parallel arrays
     - Biomes now use the tidy container classes BiomeTerrain and BiomeProp to define themselves.

--- src/world/AreaFactory.java
+++ src/world/AreaFactory.java
@@ -6,6 +6,8 @@ import game.physical.Physical;
 import thing.ThingFactory;
 import utils.Dimension;
 
+import java.util.List;
+
 /**
  *
  */
@@ -22,7 +24,7 @@ class AreaFactory {
 
     // Get a WeightMap
     final WeightMap terrainWeightMap =
-        WeightMapFactory.generateWithCrawler(areaSizeInSquares, biome.terrainWeights);
+        WeightMapFactory.generateWithCrawler(areaSizeInSquares, biome.getTerrainWeights());
 
     // Generate Features
     final Physical[][] physicals = generateFeatures(biome, terrainWeightMap, width, height);
@@ -33,7 +35,7 @@ class AreaFactory {
     for (int y = 0; y < height; y++) {
       for (int x = 0; x < width; x++) {
 
-        final String terrainTypeID = biome.terrainTypeIDs[terrainWeightMap.weightMap[y][x]];
+        final String terrainTypeID = biome.getTerrainTypeByIndex(terrainWeightMap.weightMap[y][x]);
         final Terrain terrain =
             GameResources.getTerrainTypeByID(terrainTypeID).getRandomVariation();
         final Square square = new Square(terrain);
@@ -54,31 +56,34 @@ class AreaFactory {
   private static Physical[][] generateFeatures(Biome biome, WeightMap terrainWeightMap, int width,
       int height) {
 
-    Physical[][] physicals = new Physical[height][width];
+    final Physical[][] physicals = new Physical[height][width];
 
-    for (int terrainTypeIndex = 0; terrainTypeIndex < biome.terrainTypeIDs.length;
-        terrainTypeIndex++) {
+    // For each terrain classification in the biome...
+    final List<BiomeTerrain> biomeTerrainList = biome.getBiomeTerrainList();
+    for (int terrainIndex = 0; terrainIndex < biomeTerrainList.size(); terrainIndex++) {
+      final BiomeTerrain biomeTerrain = biomeTerrainList.get(terrainIndex);
 
-      for (int featureIndex = 0; featureIndex < biome.featureIDs[terrainTypeIndex].length;
-          featureIndex++) {
+      // For each feature classification in the terrain...
+      final List<BiomeProp> features = biomeTerrain.getBiomeProps();
+      for (int featureIndex = 0; featureIndex < features.size(); featureIndex++) {
+        final BiomeProp biomeProp = features.get(featureIndex);
 
-        String featureID = biome.featureIDs[terrainTypeIndex][featureIndex];
+        // Retrieve the feature ID and calculate how many of the feature we should try to place.
+        final String featureID = biomeProp.getThingTemplateID();
+        int featureCount =
+            (int) (terrainWeightMap.distribution[terrainIndex] * biomeProp.getFrequency());
 
-        int featureCount = (int) (terrainWeightMap.distribution[terrainTypeIndex]
-            * biome.featureFrequencies[terrainTypeIndex][featureIndex]);
-
-        // pick random points until we've picked a matching terrain that has no physicals, then
-        //   place the feature and continue the search until we've placed all the features or
-        //   searched for too long
+        // Pick random points until we've picked a matching terrain that has no physicals, then
+        // place the feature and continue the search--until we've placed all the features or we've
+        // searched for too long.
         int searchLimit = 10000;
-
         while (featureCount > 0 && searchLimit > 0) {
           searchLimit--;
 
           int x = Game.RANDOM.nextInt(width);
           int y = Game.RANDOM.nextInt(height);
 
-          if (terrainWeightMap.weightMap[y][x] == terrainTypeIndex && physicals[y][x] == null) {
+          if (terrainWeightMap.weightMap[y][x] == terrainIndex && physicals[y][x] == null) {
             physicals[y][x] = ThingFactory.makeThing(featureID);
             featureCount--;
             searchLimit = 10000;
@@ -89,6 +94,7 @@ class AreaFactory {
         }
       }
     }
+
     return physicals;
   }
 }

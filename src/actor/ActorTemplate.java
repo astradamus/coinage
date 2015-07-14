5cfb195d91198952a6c46c31a0cf2038640613b4
Alexander Strada <astrada2023@gmail.com>
2015-07-13 21:39:20 -0500

    refactor: decouple parsing operations from actor/thing templates
     - New ResourceParser class unifies all overlapping operations under ResourceParser.Physicals, and all operations specific to Things or Actors under ResourceParser.Things and ResourceParser.Actors, respectively.

--- src/actor/ActorTemplate.java
+++ src/actor/ActorTemplate.java
@@ -1,13 +1,11 @@
 package actor;
 
 import actor.attribute.AttributeRange;
-import actor.attribute.Rank;
+import game.io.ResourceParser;
 import game.physical.Appearance;
 import game.physical.PhysicalFlag;
 
-import java.awt.Color;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.Map;
@@ -31,148 +29,9 @@ public class ActorTemplate {
    */
   public ActorTemplate(Map<String, String> templateMap) throws IOException {
     this.name = templateMap.get("name");
-    this.flags = parseFlags(templateMap);
-    this.baseAttributeRanges = parseAttributeRanges(templateMap);
+    this.flags = ResourceParser.Physicals.parseFlags(templateMap.get("flags"));
+    this.baseAttributeRanges = ResourceParser.Actors.buildAttributeRanges(templateMap);
     this.naturalWeaponID = templateMap.get("natural_weapon_id");
-    this.appearance = parseAppearance(templateMap);
-  }
-
-
-  /**
-   * Attempts to extract an appearance from the given template map.
-   *
-   * @throws IOException If any invalid input is encountered.
-   */
-  private Appearance parseAppearance(Map<String, String> templateMap) throws IOException {
-    final char symbol = parseSymbol(templateMap.get("symbol"));
-    final Color color = parseColor(templateMap.get("color"));
-    final Color bgcolor = parseColor(templateMap.get("bgcolor"));
-    return new Appearance(symbol, color, bgcolor, Appearance.VISUAL_PRIORITY__ACTORS);
-  }
-
-
-  /**
-   * Attempts to extract a symbol (ASCII representation) from the given template map.
-   *
-   * @throws IOException If any invalid input is encountered.
-   */
-  private char parseSymbol(String s) throws IOException {
-
-    // First try to parse the input as an ASCII character code.
-    try {
-      return (char) Integer.parseInt(s);
-    }
-    catch (NumberFormatException e) {
-
-      // If we can't, try to parse it as a literal character.
-      try {
-        return s.charAt(0);
-      }
-      catch (IndexOutOfBoundsException iob) {
-        throw new IOException("Could not parse symbol \"" + s + "\" due to invalid formatting.");
-      }
-    }
-  }
-
-
-  /**
-   * Attempts to extract a list of colors from the given template map. Colors must come in the
-   * format R:G:B, where each letter is a value between 0-255.
-   *
-   * @throws IOException If any invalid input is encountered.
-   */
-  private Color parseColor(String colorString) throws IOException {
-    final String[] rgbStrings = colorString.split(":");
-    final int[] rgb = new int[3];
-
-    try {
-      for (int i = 0; i < rgbStrings.length; i++) {
-        final String value = rgbStrings[i];
-        rgb[i] = Integer.parseInt(value);
-      }
-      return new Color(rgb[0], rgb[1], rgb[2]);
-    }
-    catch (IllegalArgumentException | IndexOutOfBoundsException ex) {
-      ex.printStackTrace();
-      throw new IOException(
-          "Could not parse color \"" + colorString + "\" due to invalid formatting.");
-    }
-  }
-
-
-  /**
-   * Attempts to extract a list of attribute ranges from the given template map.
-   *
-   * @throws IOException If any invalid input is encountered.
-   */
-  private List<AttributeRange> parseAttributeRanges(Map<String, String> map) throws IOException {
-    return Arrays
-        .asList(parseAttributeRange(map.get("muscle")), parseAttributeRange(map.get("grit")),
-            parseAttributeRange(map.get("reflex")), parseAttributeRange(map.get("talent")),
-            parseAttributeRange(map.get("perception")), parseAttributeRange(map.get("charm")));
-  }
-
-
-  /**
-   * Attempts to parse an attribute range from the given string. Only two formats are acceptable:
-   * single values, such as "7", which will result in a fixed range (7-7); or a pair of values
-   * separated by a hyphen, such as "6-8", resulting in (6-8).
-   *
-   * @throws IOException If any invalid input is encountered.
-   */
-  private AttributeRange parseAttributeRange(String rangeString) throws IOException {
-
-    final String[] split = rangeString.split("-");
-    final int[] ints;
-
-    // Parse single-value range.
-    if (split.length == 1) {
-      final int singleValue = Integer.parseInt(split[0]);
-      ints = new int[] { singleValue, singleValue };
-    }
-
-    // Parse double-value range.
-    else if (split.length == 2) {
-      ints = new int[] { Integer.parseInt(split[0]), Integer.parseInt(split[1]) };
-    }
-
-    // Can only parse single- or double-value ranges.
-    else {
-      throw new IOException(
-          "Could not parse attribute range \"" + rangeString + "\" due to invalid "
-              + "formatting.");
-    }
-
-    return new AttributeRange(Rank.values()[ints[0]], Rank.values()[ints[1]]);
-  }
-
-
-  /**
-   * Attempts to extract a list of flags from the given template map. Multiple flags must be
-   * space-separated to be recognized.
-   *
-   * @throws IOException If any invalid input is encountered.
-   */
-  private EnumSet<PhysicalFlag> parseFlags(Map<String, String> templateMap) throws IOException {
-
-    final EnumSet<PhysicalFlag> flags = EnumSet.noneOf(PhysicalFlag.class);
-
-    final String flagsString = templateMap.get("flags");
-
-    if (flagsString != null) {
-      final String[] splitFlagStrings = flagsString.split(" ");
-
-      for (final String s : splitFlagStrings) {
-        try {
-          flags.add(PhysicalFlag.valueOf(s));
-        }
-        catch (IllegalArgumentException e) {
-          e.printStackTrace();
-          throw new IOException("Could not parse flag \"" + s + "\" because it is not recognized.");
-        }
-      }
-    }
-
-    return flags;
+    this.appearance = ResourceParser.Actors.buildActorAppearance(templateMap);
   }
 }
--- src/thing/ThingTemplate.java
+++ src/thing/ThingTemplate.java
@@ -1,13 +1,11 @@
 package thing;
 
-import actor.stats.DamageType;
 import game.Game;
+import game.io.ResourceParser;
 import game.physical.Appearance;
 import game.physical.PhysicalFlag;
 
-import java.awt.Color;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.Map;
@@ -30,160 +28,13 @@ public class ThingTemplate {
    */
   public ThingTemplate(Map<String, String> templateMap) throws IOException {
     this.name = templateMap.get("name");
-    this.appearances = parseAppearances(templateMap);
-    this.flags = parseFlags(templateMap);
-    this.weaponComponent = parseWeaponComponent(templateMap);
+    this.appearances = ResourceParser.Things.buildThingAppearances(templateMap);
+    this.flags = ResourceParser.Physicals.parseFlags(templateMap.get("flags"));
+    this.weaponComponent = ResourceParser.Things.buildWeaponComponent(templateMap);
   }
 
 
   Appearance getRandomAppearance() {
     return appearances.get(Game.RANDOM.nextInt(appearances.size()));
-  }
-
-
-  /**
-   * Attempts to extract a list of appearances from the given template map. Multiple symbol and
-   * color entries must be space-separated to be recognized.
-   *
-   * @throws IOException If any invalid input is encountered.
-   */
-  private List<Appearance> parseAppearances(Map<String, String> templateMap) throws IOException {
-
-    final List<Appearance> appearances = new ArrayList<>();
-
-    final String[] symbolStrings = templateMap.get("symbols").split(" ");
-    final List<Character> symbols = parseSymbols(symbolStrings);
-
-    final String[] colorStrings = templateMap.get("colors").split(" ");
-    final List<Color> colors = parseColors(colorStrings);
-
-    for (char character : symbols) {
-      for (Color color : colors) {
-        appearances.add(new Appearance(character, color, null, Appearance.VISUAL_PRIORITY__THINGS));
-      }
-    }
-
-    return appearances;
-  }
-
-
-  /**
-   * Attempts to extract a list of symbols (ASCII representations) from the given template map.
-   *
-   * @throws IOException If any invalid input is encountered.
-   */
-  private List<Character> parseSymbols(String[] symbolStrings) throws IOException {
-
-    final List<Character> symbols = new ArrayList<>();
-
-    for (final String s : symbolStrings) {
-
-      // First try to parse the input as an ASCII character code.
-      try {
-        symbols.add((char) Integer.parseInt(s));
-      }
-      catch (NumberFormatException e) {
-
-        // If we can't, try to parse it as a literal character.
-        try {
-          symbols.add(s.charAt(0));
-        }
-        catch (IndexOutOfBoundsException iob) {
-          throw new IOException("Could not parse symbol \"" + s + "\" due to invalid formatting.");
-        }
-      }
-    }
-
-    return symbols;
-  }
-
-
-  /**
-   * Attempts to extract a list of colors from the given template map. Colors must come in the
-   * format R:G:B, where each letter is a value between 0-255.
-   *
-   * @throws IOException If any invalid input is encountered.
-   */
-  private List<Color> parseColors(String[] colorStrings) throws IOException {
-
-    final List<Color> colors = new ArrayList<>();
-
-    for (final String s : colorStrings) {
-
-      final String[] rgbStrings = s.split(":");
-      final int[] rgb = new int[3];
-
-      try {
-        for (int i = 0; i < rgbStrings.length; i++) {
-          final String value = rgbStrings[i];
-          rgb[i] = Integer.parseInt(value);
-        }
-        colors.add(new Color(rgb[0], rgb[1], rgb[2]));
-      }
-      catch (IllegalArgumentException | IndexOutOfBoundsException ex) {
-        ex.printStackTrace();
-        throw new IOException("Could not parse color \"" + s + "\" due to invalid formatting.");
-      }
-    }
-
-    return colors;
-  }
-
-
-  /**
-   * Attempts to extract a list of flags from the given template map. Multiple flags must be
-   * space-separated to be recognized.
-   *
-   * @throws IOException If any invalid input is encountered.
-   */
-  private EnumSet<PhysicalFlag> parseFlags(Map<String, String> templateMap) throws IOException {
-
-    final EnumSet<PhysicalFlag> flags = EnumSet.noneOf(PhysicalFlag.class);
-
-    final String flagsString = templateMap.get("flags");
-
-    if (flagsString != null) {
-      final String[] splitFlagStrings = flagsString.split(" ");
-
-      for (final String s : splitFlagStrings) {
-        try {
-          flags.add(PhysicalFlag.valueOf(s));
-        }
-        catch (IllegalArgumentException e) {
-          e.printStackTrace();
-          throw new IOException("Could not parse flag \"" + s + "\" because it is not recognized.");
-        }
-      }
-    }
-
-    return flags;
-  }
-
-
-  /**
-   * Attempts to build a weapon component from the given template map, returning null if it fails.
-   */
-  private WeaponComponent parseWeaponComponent(Map<String, String> templateMap) {
-
-    try {
-
-      final DamageType damageType = DamageType.valueOf(templateMap.get("damage_type"));
-
-      final int damage = Integer.parseInt(templateMap.get("damage"));
-      final double damageBonusMultFromMuscle = Double.parseDouble(templateMap.get("muscle_bonus"));
-      final double damageConsistency = Double.parseDouble(templateMap.get("dmg_consistency"));
-
-      final int attackSpeed = Integer.parseInt(templateMap.get("attack_speed"));
-      final double reflexAttackBonus = Double.parseDouble(templateMap.get("reflex_atk_bonus"));
-
-      final int recoverySpeed = Integer.parseInt(templateMap.get("recovery_speed"));
-      final double reflexRecoveryBonus = Double.parseDouble(templateMap.get("reflex_rec_bonus"));
-
-      return new WeaponComponent(damageType, damage, damageBonusMultFromMuscle, damageConsistency,
-          attackSpeed, reflexAttackBonus, recoverySpeed, reflexRecoveryBonus);
-    }
-    catch (Exception e) {
-      return null; // No valid weapon data found.
-    }
   }
 }

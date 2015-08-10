ae629411e84f29d5bc63d5b4c12b721a6a4824b1
Alexander Strada <astrada2023@gmail.com>
2015-08-10 17:36:29 -0500

    refactor(MousePosition): now extends ImmutablePoint
     - Functionality is unchanged.

--- src/game/io/better_ui/widget_ui/MousePosition.java
+++ src/game/io/better_ui/widget_ui/MousePosition.java
@@ -1,36 +1,19 @@
 package game.io.better_ui.widget_ui;
 
+import utils.ImmutablePoint;
+
 /**
  *
  */
-public class MousePosition {
-
-  private final int x;
-  private final int y;
+public class MousePosition extends ImmutablePoint {
 
   private final long updateTime;
   private boolean toolTipHoverTimeReached;
 
 
   public MousePosition(int x, int y, long updateTime) {
-    this.x = x;
-    this.y = y;
+    super(x, y);
     this.updateTime = updateTime;
-  }
-
-
-  public boolean equalTo(int x, int y) {
-    return this.x == x && this.y == y;
-  }
-
-
-  public int getX() {
-    return x;
-  }
-
-
-  public int getY() {
-    return y;
   }
 
 

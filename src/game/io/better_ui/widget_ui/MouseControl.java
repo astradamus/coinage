3c9ea0d232d77bb08e00ba86c6bad504f4d94745
Alexander Strada <astrada2023@gmail.com>
2015-08-11 16:51:47 -0500

    refactor(MouseControl): marginally clean up prototype, tweak colors

--- src/game/io/better_ui/widget_ui/MouseControl.java
+++ src/game/io/better_ui/widget_ui/MouseControl.java
@@ -18,7 +18,6 @@ import javax.swing.Timer;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FontMetrics;
-import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -30,15 +29,14 @@ import java.awt.event.MouseMotionListener;
 public class MouseControl implements MouseMotionListener, MouseListener {
 
   private final Game game;
-  private GamePanel gamePanel;
-
   private final int tileSize;
   private final Dimension screenSize;
-
+  private GamePanel gamePanel;
   private Coordinate playerAreaOrigin;
   private MousePosition mouseAt;
 
   private ToolTip toolTip;
+
 
   public MouseControl(Game game, GamePanel gamePanel) {
     this.game = game;
@@ -52,105 +50,18 @@ public class MouseControl implements MouseMotionListener, MouseListener {
     this.playerAreaOrigin = playerAreaOrigin;
 
     if (toolTip == null && mouseAt != null && mouseAt.getToolTipHoverTimeReached()) {
-      toolTip = new ToolTip(makeToolTip(g, getPhysicalUnderMouse()));
+      toolTip = new ToolTip();
     }
 
     if (toolTip != null) {
 
       if (toolTip.handlesMouse && mouseAt != null) {
         g.setColor(Color.WHITE);
-        g.drawOval(mouseAt.getX()*tileSize,mouseAt.getY()*tileSize,tileSize,tileSize);
+        g.drawOval(mouseAt.getX() * tileSize, mouseAt.getY() * tileSize, tileSize, tileSize);
       }
 
-      toolTip.widget.draw(g);
+      toolTip.draw(g);
     }
-  }
-
-  private class ToolTip {
-    private final LinearLayout widget;
-    final ImmutableRectangle collapsedBox;
-
-    private boolean fading = false;
-    private boolean handlesMouse = false;
-
-
-    public ToolTip(LinearLayout widget) {
-      this.widget = widget;
-
-      final ImmutableRectangle mB = widget.getMarginBox();
-      collapsedBox =
-          mB.getAdjusted(mB.getWidth() / 2, mB.getHeight() / 2, -mB.getWidth(), -mB.getHeight());
-
-      final AnimatedWidget child = (AnimatedWidget) this.widget.getChild(0);
-      child.animateTransform(collapsedBox, mB, 350);
-      child.animateFade(0, widget.getAlpha().getAlpha(), 500);
-    }
-
-    void fadeOut() {
-      if (fading) {
-        return;
-      }
-      else {
-        fading = true;
-      }
-
-      for (Widget child : this.widget.getSubwidgets()) {
-        final AnimatedWidget anim = (AnimatedWidget) child;
-        final ImmutableRectangle mB = anim.getMarginBox();
-        anim.animateTransform(mB, collapsedBox, 275);
-        anim.animateFade(child.getAlpha().getAlpha(), 0, 300);
-      }
-
-      this.widget.animateTransform(this.widget.getMarginBox(), collapsedBox, 275);
-      this.widget.animateFade(this.widget.getAlpha().getAlpha(), 0, 300);
-
-      final Timer timer = new Timer(300, (aE) -> toolTip = null);
-      timer.setRepeats(false);
-      timer.start();
-    }
-  }
-
-
-  private LinearLayout makeToolTip(Graphics g, Physical p) {
-
-    final ImmutablePoint widgetAt =
-        new ImmutablePoint((mouseAt.getX() + 2) * tileSize, mouseAt.getY() * tileSize);
-
-    final TextWidget titleWidget =
-        new TextWidget(g.getFontMetrics(TextWidget.STANDARD_FONT), p.getName());
-
-    titleWidget.setBorder(1);
-    titleWidget.setBorderColor(p.getColor());
-
-    titleWidget.setColor(p.getColor());
-    titleWidget.setBgColor(new Color(0, 0, 0, 255));
-
-    titleWidget.getPadding().set(5, 2);
-
-    titleWidget.pack();
-    titleWidget.moveAndResize(new ImmutableRectangle(
-        widgetAt.getTranslated(0, -titleWidget.getNonContentSize().getHeight()),
-        titleWidget.getPreferredSize()));
-
-    final ImmutableRectangle marginBox = titleWidget.getMarginBox();
-    LinearLayout linearLayout = new LinearLayout(marginBox, Orientation.VERT);
-    linearLayout.add(titleWidget);
-
-    int offX = 0;
-    int offY = 0;
-
-    if (marginBox.getRight() > screenSize.getWidth()) {
-      offX = -tileSize * 3 - marginBox.getWidth();
-    }
-    if (marginBox.getBottom() > screenSize.getHeight()) {
-      offY = -marginBox.getHeight();
-    }
-
-    if (offX != 0 || offY != 0) {
-      linearLayout.move(marginBox.getOrigin().getTranslated(offX, offY));
-    }
-
-    return linearLayout;
   }
 
 
@@ -169,39 +80,16 @@ public class MouseControl implements MouseMotionListener, MouseListener {
 
 
   private Physical getPhysicalUnderMouse() {
-    return game.getWorld()
-        .getSquare(playerAreaOrigin.offset(mouseAt.getX(), mouseAt.getY())).peek();
-  }
-
-
-  @Override
-  public void mouseMoved(MouseEvent e) {
-
-    if (toolTip != null && toolTip.handlesMouse) {
-      toolTip.widget.handleMouseMoved(e);
-      return;
-    }
-
-    final int tileX = e.getX() / tileSize;
-    final int tileY = e.getY() / tileSize;
-
-    final ImmutableDimension areaSize = game.getWorld().getAreaSizeInSquares();
-    if (tileX >= areaSize.getWidth() || tileY >= areaSize.getHeight()) {
-      mouseExited(e);
-      return;
-    }
-
-    if (mouseAt == null || !mouseAt.equalTo(tileX, tileY)) {
-      setCursor(tileX, tileY);
-    }
+    return game.getWorld().getSquare(playerAreaOrigin.offset(mouseAt.getX(), mouseAt.getY()))
+        .peek();
   }
 
 
   @Override
   public void mouseClicked(MouseEvent e) {
     if (toolTip != null && toolTip.handlesMouse) {
-      if (toolTip.widget.getMarginBox().contains(e.getPoint())) {
-        toolTip.widget.handleMouseClicked(e);
+      if (toolTip.getMarginBox().contains(e.getPoint())) {
+        toolTip.handleMouseClicked(e);
       }
       else {
         clearCursor();
@@ -214,61 +102,43 @@ public class MouseControl implements MouseMotionListener, MouseListener {
     }
 
     toolTip.handlesMouse = true;
-
-    final LinearLayout widget = toolTip.widget;
-    widget.setBorder(widget.getChild(0).getBorder());
-    widget.setBorderColor(widget.getChild(0).getBorderColor());
-    final ImmutableRectangle mB = widget.getMarginBox();
+    toolTip.setBorder(toolTip.getChild(0).getBorder());
+    toolTip.setBorderColor(toolTip.getChild(0).getBorderColor());
+    final ImmutableRectangle mB = toolTip.getMarginBox();
 
     final ImmutableRectangle box;
     final int width = Math.max(242, mB.getWidth());
-    final ImmutableRectangle newBox = new ImmutableRectangle(mB.getX(), mB.getY(), width, mB.getHeight());
-
+    final ImmutableRectangle newBox =
+        new ImmutableRectangle(mB.getX(), mB.getY(), width, mB.getHeight());
 
     int adjX = 0;
 
     if (newBox.getRight() > screenSize.width) {
-      adjX = -newBox.getWidth() -tileSize*3;
+      adjX = -newBox.getWidth() - tileSize * 3;
     }
 
-    box = newBox.getAdjusted(adjX,0,0,0);
+    box = newBox.getAdjusted(adjX, 0, 0, 0);
 
     final LinearLayout tools = new LinearLayout(null, Orientation.HORZ);
 
     final FontMetrics fM = gamePanel.getFontMetrics(TextWidget.STANDARD_FONT);
 
-    final Button widget1 = new Button(fM, "Pick Up");
-    widget1.setLayoutWeight(1);
-    final Color green = new Color(0, 255, 0, 175).darker().darker();
-    widget1.setBorderColor(green);
-    widget1.setBgColor(green.darker());
-    widget1.setBorder(2);
-    tools.add(widget1);
+    final Color red = new Color(100, 0, 0, 225).darker().darker();
+    final Color green = new Color(0, 100, 0, 225).darker().darker();
+    final Color blue = new Color(0, 0, 100, 225).darker().darker();
 
-    final Button widget2 = new Button(fM, "Attack");
-    widget2.setLayoutWeight(1);
-    final Color red = new Color(255, 0, 0, 175).darker().darker();
-    widget2.setBorderColor(red);
-    widget2.setBgColor(red.darker());
-    widget2.setBorder(2);
-    tools.add(widget2);
-
-    final Button widget3 = new Button(fM, "Skill");
-    widget3.setLayoutWeight(1);
-    final Color blue = new Color(0, 0, 255, 175).darker().darker();
-    widget3.setBorderColor(blue);
-    widget3.setBgColor(blue.darker());
-    widget3.setBorder(2);
-    tools.add(widget3);
+    tools.add(new ToolBoxButton(green, fM, "Pick\nUp"));
+    tools.add(new ToolBoxButton(red, fM, "Attack\nHere"));
+    tools.add(new ToolBoxButton(blue, fM, "Use\nSkill"));
 
     tools.setLayoutWeight(1);
-    widget.animateTransform(mB, box, 250);
+    toolTip.animateTransform(mB, box, 250);
 
-    toolTip.widget.add(tools);
+    toolTip.add(tools);
 
     final Timer timer = new Timer(250, e1 -> {
       final ImmutableRectangle nextBox =
-          new ImmutableRectangle(box.getX(), box.getY(), box.getWidth(), 100);
+          new ImmutableRectangle(box.getX(), box.getY(), box.getWidth(), 75);
       final ImmutableRectangle next;
 
       int adjY = 0;
@@ -276,9 +146,9 @@ public class MouseControl implements MouseMotionListener, MouseListener {
         adjY = screenSize.height - nextBox.getBottom();
       }
 
-      next = nextBox.getAdjusted(0,adjY,0,0);
+      next = nextBox.getAdjusted(0, adjY, 0, 0);
 
-      widget.animateTransform(widget.getMarginBox(), next, 250);
+      toolTip.animateTransform(toolTip.getMarginBox(), next, 250);
     });
     timer.setRepeats(false);
     timer.start();
@@ -305,4 +175,119 @@ public class MouseControl implements MouseMotionListener, MouseListener {
 
   @Override
   public void mouseDragged(MouseEvent e) { }
+
+
+  @Override
+  public void mouseMoved(MouseEvent e) {
+
+    if (toolTip != null && toolTip.handlesMouse) {
+      toolTip.handleMouseMoved(e);
+      return;
+    }
+
+    final int tileX = e.getX() / tileSize;
+    final int tileY = e.getY() / tileSize;
+
+    final ImmutableDimension areaSize = game.getWorld().getAreaSizeInSquares();
+    if (tileX >= areaSize.getWidth() || tileY >= areaSize.getHeight()) {
+      mouseExited(e);
+      return;
+    }
+
+    if (mouseAt == null || !mouseAt.equalTo(tileX, tileY)) {
+      setCursor(tileX, tileY);
+    }
+  }
+
+  class ToolTip extends LinearLayout {
+    final ImmutableRectangle collapsedBox;
+
+    private boolean fading = false;
+    private boolean handlesMouse = false;
+
+
+    public ToolTip() {
+      super(null, Orientation.VERT);
+
+      final ImmutablePoint widgetAt =
+          new ImmutablePoint((mouseAt.getX() + 2) * tileSize, mouseAt.getY() * tileSize);
+
+      final Physical p = getPhysicalUnderMouse();
+
+      final TextWidget titleWidget =
+          new TextWidget(gamePanel.getFontMetrics(TextWidget.STANDARD_FONT), p.getName());
+
+      titleWidget.setBorder(1);
+      titleWidget.setBorderColor(p.getColor());
+
+      titleWidget.setColor(p.getColor());
+      titleWidget.setBgColor(new Color(0, 0, 0, 255));
+
+      titleWidget.getPadding().set(5, 2);
+
+      titleWidget.pack();
+      titleWidget.moveAndResize(new ImmutableRectangle(
+          widgetAt.getTranslated(0, -titleWidget.getNonContentSize().getHeight()),
+          titleWidget.getPreferredSize()));
+
+      final ImmutableRectangle marginBox = titleWidget.getMarginBox();
+      moveAndResize(marginBox);
+      add(titleWidget);
+
+      int offX = 0;
+      int offY = 0;
+
+      if (marginBox.getRight() > screenSize.getWidth()) {
+        offX = -tileSize * 3 - marginBox.getWidth();
+      }
+      if (marginBox.getBottom() > screenSize.getHeight()) {
+        offY = -marginBox.getHeight();
+      }
+
+      if (offX != 0 || offY != 0) {
+        move(marginBox.getOrigin().getTranslated(offX, offY));
+      }
+
+      final ImmutableRectangle mB = getMarginBox();
+      collapsedBox =
+          mB.getAdjusted(mB.getWidth() / 2, mB.getHeight() / 2, -mB.getWidth(), -mB.getHeight());
+
+      final AnimatedWidget child = (AnimatedWidget) this.getChild(0);
+      child.animateTransform(collapsedBox, mB, 350);
+      child.animateFade(0, getAlpha().getAlpha(), 500);
+    }
+
+
+    void fadeOut() {
+      if (fading) {
+        return;
+      }
+      else {
+        fading = true;
+      }
+
+      for (Widget child : this.getSubwidgets()) {
+        final AnimatedWidget anim = (AnimatedWidget) child;
+        final ImmutableRectangle mB = anim.getMarginBox();
+        anim.animateTransform(mB, collapsedBox, 275);
+        anim.animateFade(child.getAlpha().getAlpha(), 0, 300);
+      }
+
+      this.animateTransform(this.getMarginBox(), collapsedBox, 275);
+      this.animateFade(this.getAlpha().getAlpha(), 0, 300);
+
+      final Timer timer = new Timer(300, (aE) -> toolTip = null);
+      timer.setRepeats(false);
+      timer.start();
+    }
+  }
+  class ToolBoxButton extends Button {
+    public ToolBoxButton(Color bgColor, FontMetrics fontMetrics, String string) {
+      super(fontMetrics, string);
+      setLayoutWeight(1);
+      setBorder(2);
+      setBorderColor(bgColor);
+      setBgColor(bgColor.darker());
+    }
+  }
 }
